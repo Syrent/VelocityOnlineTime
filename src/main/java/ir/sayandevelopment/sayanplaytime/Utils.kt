52d66ef4df9fbc1b4cd2adce6764dc7cc0b95350
Syrent <Syrent2356@gmail.com>
2022-08-27 22:12:24 +0430

    Change utils language to Kotlin and move `formattedTime` to Utils.kt

---
+++ src/main/java/ir/sayandevelopment/sayanplaytime/Utils.kt
@@ -0,0 +1,14 @@
+package ir.sayandevelopment.sayanplaytime
+
+object Utils {
+    fun capitalize(string: String): String {
+        return string.uppercase()[0].toString() + string.lowercase().substring(1)
+    }
+
+    fun formatTime(total_time: Long): String {
+        val seconds = total_time / 1000
+        val hours = (seconds / 3600).toInt()
+        val minutes = (seconds % 3600 / 60).toInt()
+        return String.format("%sh %sm", hours, minutes)
+    }
+}
