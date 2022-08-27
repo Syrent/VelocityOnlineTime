5bf4bc19973426180af9e2c2ff90d3728d1ec6bb
Syrent <Syrent2356@gmail.com>
2022-08-27 22:12:39 +0430

    Remove Utils.java

--- src/main/java/ir/sayandevelopment/sayanplaytime/Utils.java
+++
@@ -1,7 +0,0 @@
-package ir.sayandevelopment.sayanplaytime;
-
-public class Utils {
-    public static String capitalize(String string) {
-        return string.toUpperCase().charAt(0) + string.toLowerCase().substring(1);
-    }
-}
